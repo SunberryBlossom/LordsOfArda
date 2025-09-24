ca9de33c3715417610ff7bd0afd0227a00208b40
Johannes Flodin <jannis020609@gmail.com>
2025-09-24 19:57:48 +0200

    added StartUI,AssetManager,Enum

---
+++ StartUI.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LordsOfArda
+{
+    internal class StartUI
+    {
+        public void IntroScene(string logo)
+        {
+            Console.WriteLine(logo);
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+            Console.Clear();
+        }
+        public void StartMenu()
+        {
+            string logo = AssetManager.GetAsciiArt(ArtType.Logo);
+            IntroScene(logo);
+            string[] mainMenuOptions = { "New Game", "Load Game","Options","Credits", "Exit" };
+            bool isContinue = true;
+            while (isContinue)
+            {
+                StartMenuOptions selectedOption = Menu.ReadOption<StartMenuOptions>(logo, mainMenuOptions);
+                switch (selectedOption)
+                {
+                    case StartMenuOptions.NewGame:
+                        break;
+                    case StartMenuOptions.LoadGame:
+                        break;
+                    case StartMenuOptions.Options:
+                        break;
+                    case StartMenuOptions.Credits:
+                        break;
+                    case StartMenuOptions.Exit:
+                        isContinue = false;
+                        break;
+                }
+            }
+
+        }
+    }
+}
