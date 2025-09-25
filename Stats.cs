9c0cc38377576f20a75ff1cf45235aa37980c99f
Gabriel Kassarp <gkassarp@gmail.com>
2025-09-25 17:49:55 +0200

    smol baseline to characterclass

---
+++ Stats.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LordsOfArda
+{
+    internal class Stats
+    {
+            public int Strength { get; set; }
+            public int Agility { get; set; }
+            public int Intelligence { get; set; }
+            public int Endurance { get; set; }
+
+            public Stats(int strength, int agility, int intelligence, int endurance)
+            {
+                Strength = strength;
+                Agility = agility;
+                Intelligence = intelligence;
+                Endurance = endurance;
+            }
+
+            public override string ToString()
+            {
+                return $"STR: {Strength}, AGI: {Agility}, INT: {Intelligence}, END: {Endurance}";
+            }
+        }
+    
+}
