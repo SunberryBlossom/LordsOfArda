9704a81aa5c9cdb3c708d1b3fb873939c774b34e
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2025-09-25 18:36:43 +0200

    Added basic folder structure for elven classes

---
+++ Beings/Races/Elves/Quendi/Eldar/Eldar.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LordsOfArda.Beings.Races.Elves.Quendi.Eldar
+{
+    internal class Eldar : Quendi
+    {
+        public Eldar(string name, int age, string title, string origin) : base(name, age, title, origin) 
+        { 
+        }
+    }
+}
