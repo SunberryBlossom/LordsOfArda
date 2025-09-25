c45a887e1ab9afe4e55b59b5398b750582b2d8cb
Gabriel Kassarp <gkassarp@gmail.com>
2025-09-25 17:49:55 +0200

    smol baseline to characterclass

--- Program.cs
+++ Program.cs
@@ -5,6 +5,13 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            Stats heroStats = new Stats(strength: 10, agility: 8, intelligence: 7, endurance: 9);
+            Character test = new Character("Bobbey", 10, "Male", "", "The Shire",heroStats );
+
+            test.PrintInfo();
         }
+
+       
     }
 }
