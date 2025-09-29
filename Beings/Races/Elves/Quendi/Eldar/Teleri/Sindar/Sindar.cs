ec067a598c2f2c61b9ee9b4f361c39756e8a6bf5
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2025-09-29 13:41:55 +0200

    removed .gitkeep files where class files have been added. Added a Sindar class file

---
+++ Beings/Races/Elves/Quendi/Eldar/Teleri/Sindar/Sindar.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LordsOfArda.Beings.Races.Elves.Quendi.Eldar.Teleri.Sindar
+{
+    internal class Sindar : Eldar
+    {
+        string _familyTree = "Thingol";
+
+        public string FamilyTree
+        {
+            get { return _familyTree; }
+        }
+
+        public Sindar(string name, int age, string title, string origin) : base(name, age, title, origin)
+        {
+        }
+    }
+}
