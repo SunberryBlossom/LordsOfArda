255a4bcd021432a72b75a46dc2006f74797a15f8
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2025-09-29 12:56:32 +0200

    Removes calaquendi as class, replaced as a bool instead in sub classes. Added Noldor class

---
+++ Beings/Races/Elves/Quendi/Eldar/Noldor/Noldor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LordsOfArda.Beings.Races.Elves.Quendi.Eldar.Noldor
+{
+    internal class Noldor : Eldar
+    {
+        private bool _isCalaquendi = true;
+
+        public bool IsCalaquendi 
+        {
+            get { return _isCalaquendi; } 
+            set { Console.WriteLine("All of the Noldors are considered Calaquendi."); } 
+        }
+
+        public Noldor(string name, int age, string title, string origin) : base(name, age, title, origin)
+        {
+
+        }
+    }
+}
