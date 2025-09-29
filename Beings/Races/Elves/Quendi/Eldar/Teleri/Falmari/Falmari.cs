b8230d570c60cdda46d0f56ff58b5f03b42ef936
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2025-09-29 13:38:58 +0200

    Renaming of folders for better praxis

---
+++ Beings/Races/Elves/Quendi/Eldar/Teleri/Falmari/Falmari.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LordsOfArda.Beings.Races.Elves.Quendi.Eldar.Teleri.Falmari__Sea_elves_
+{
+    internal class Falmari : Eldar
+    {
+        string _familyTree = "Olwë";
+
+        public string FamilyTree
+        {
+            get { return _familyTree; }
+        }
+
+        public Falmari(string name, int age, string title, string origin) : base(name, age, title, origin)
+        {
+        }
+    }
+}
