b2f45ec1ec15c116ec4af2897bf3a34cdef2fab2
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2025-09-29 13:04:37 +0200

    Added Family tree property to Noldor class

--- Beings/Races/Elves/Quendi/Eldar/Noldor/Noldor.cs
+++ Beings/Races/Elves/Quendi/Eldar/Noldor/Noldor.cs
@@ -9,6 +9,7 @@ namespace LordsOfArda.Beings.Races.Elves.Quendi.Eldar.Noldor
     internal class Noldor : Eldar
     {
         private bool _isCalaquendi = true;
+        private string _familyTree;
 
         public bool IsCalaquendi 
         {
@@ -16,9 +17,27 @@ namespace LordsOfArda.Beings.Races.Elves.Quendi.Eldar.Noldor
             set { Console.WriteLine("All of the Noldors are considered Calaquendi."); } 
         }
 
-        public Noldor(string name, int age, string title, string origin) : base(name, age, title, origin)
-        {
+        public string FamilyTree 
+        { 
+            get { return _familyTree; } 
+            
+            set
+            {
+                if(value == "Fëanor" || value == "Fingolfin" || value == "Finarfin")
+                {
+                    _familyTree = value;
+                }
+                else
+                {
+                    Console.WriteLine("The three households of the Noldor are:\nFëanor\nFingolfin\nFinarfin\n\nPlease try assign one of these as your Family Tree.");
+                }
+            }
+            
+        }
 
+        public Noldor(string familyTree, string name, int age, string title, string origin) : base(name, age, title, origin)
+        {
+            FamilyTree = familyTree;
         }
     }
 }
