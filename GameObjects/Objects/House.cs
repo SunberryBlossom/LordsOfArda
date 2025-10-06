df239b2d3793e1b55bc8eb010403cd3f1bab18b0
Johannes Flodin <jannis020609@gmail.com>
2025-10-06 14:34:10 +0200

    grid testing

---
+++ GameObjects/Objects/House.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LordsOfArda.GameObjects.Objects
+{
+    public class House : GameObject
+    {
+        public override string CharacterSign => "🏠";
+        public int width = 3;
+        public int height = 3;
+        public House(int startX = 1, int startY = 1) : base(startX,startY)
+        {
+        
+        }
+    }
+}
